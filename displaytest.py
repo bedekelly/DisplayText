baa4f63caad23d3ec155b4ab884bca4945751c74
Bede Kelly <bedekelly@users.noreply.github.com>
2014-06-25 19:20:57 +0100

    Update displaytest.py

--- displaytest.py
+++ displaytest.py
@@ -3,4 +3,5 @@ from DisplayText import Display
 with open("prose") as text_file:
 	text = text_file.read()
 	text_display = Display(text, ok_button="Accept")
+	# ok_button is optional, C-c always closes the display without errors.
 	text_display.show()
